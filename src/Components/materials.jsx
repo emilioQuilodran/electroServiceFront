29fd430caeb737dbca6fe18e91fcb3ffdedad802
Santiago Corino <santicorino@gmail.com>
2023-08-18 19:15:29 -0300

    Primera estructura

---
+++ src/Components/materials.jsx
@@ -0,0 +1,11 @@
+
+const Materials = () => {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h1>Materials</h1>
+    </div>
+  )
+}
+
+export default Materials
---
+++ src/Components/providers.jsx
@@ -0,0 +1,11 @@
+
+const Porviders = () => {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h1>Porviders</h1>
+    </div>
+  )
+}
+
+export default Porviders
