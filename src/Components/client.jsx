b6d7f1928421a78c4149986d7ae02bb1fdab6330
Santiago Corino <santicorino@gmail.com>
2023-08-22 19:24:40 -0300

    Agregada la lista de items

--- src/Components/client.jsx
+++ src/Components/client.jsx
@@ -4,6 +4,12 @@ const Client = () => {
     <div>
       <h1>Hola</h1>
       <h1>Client</h1>
+      <ul>
+        <li>Soy un item</li>
+        <li>Soy otro item</li>
+        <li>Soy un item más</li>
+        <li>Soy el último item</li>
+      </ul>
     </div>
   )
 }
