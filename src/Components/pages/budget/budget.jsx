94ed6b859e88d0040a3f3ac99ed33a02f28657b4
maxiv03 <nocqponer04@gmail.com>
2023-08-29 04:29:27 -0300

    jsx form structure created

--- src/Components/pages/budget/budget.jsx
+++ src/Components/pages/budget/budget.jsx
@@ -1,17 +1,31 @@
 
 const Budget = () => {
   return (
-    <div>
-      <h1>Hola</h1>
-      <h1>Budget</h1>
-      <ul>
-        <li>Caro</li>
-        <li>Carisimo</li>
-        <li>Incomprable</li>
-        <li>test 2</li>
-        <li>test 1</li>
-      </ul>
-    </div>
+    <form action="" method="get">
+      <fieldset>
+        <h1>Budget</h1><hr/><br/>
+        <div>
+          <label htmlFor="id-reception"> ID recepción: </label>
+          <input id="id-reception" type="text" value="" size="28"/>
+        </div>
+        <div>
+          <label htmlFor="description">Descripción de la reparación: </label><br/>
+          <textarea id="description" rows="7" cols="41"></textarea>
+        </div>
+        <div>
+          <label htmlFor="materials-list">Lista de materiales: </label><br/>
+          <textarea id="materials-list" rows="7" cols="41">
+          </textarea>
+        </div>
+        <div>
+          <label htmlFor="warranty-end-date">Fecha finalización de garantía: </label>
+          <input id="warranty-end-date" type="date"/>
+        </div>
+        <br/>
+        <input type="submit" value="Enviar"></input>
+        <input type="button" value="Cancelar"/>
+      </fieldset>
+    </form>
   )
 }
 
